126ad276b7f2c73318ca48995b4220d405f6f51f
Erika <erika.hughes@dbspublishing.com>
2016-03-21 20:38:32 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wowify::Application.config.secret_key_base = '616b3ae30e81ce733775887267cb1bce8c8d744c41ea001767b37cabdd90d4308933f853f61deef1f812654ba79e0a41d13d97dab9959b7b591e211ade51f706'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wowify::Application.config.session_store :cookie_store, key: '_wowify_session'

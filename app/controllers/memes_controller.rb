9b6fa3553984679db56285032e75926e43cf47ce
Erika <erika.hughes@dbspublishing.com>
2016-03-21 22:06:26 +0000

    added Modal for quote submission and stylized some things

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -2,4 +2,15 @@ class MemesController < ApplicationController
   def index
     @meme = Meme.order("RANDOM()").first
   end
+
+  def create
+    Meme.create(meme_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def meme_params
+    params.require(:meme).permit(:wow, :such)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Wowify::Application.routes.draw do
   root 'memes#index'
+
+  resources :memes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
